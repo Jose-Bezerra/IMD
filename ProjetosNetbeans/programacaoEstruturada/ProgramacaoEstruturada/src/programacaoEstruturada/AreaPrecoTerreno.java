5c456508b2270aca71fc723bda975a2208d77b58
José Bezerra <josbezfi@gmail.com>
2020-11-29 13:12:43 -0300

    Adicionado exercícios novos na pasta ProjetosNetbeans

---
+++ ProjetosNetbeans/programacaoEstruturada/ProgramacaoEstruturada/src/programacaoEstruturada/AreaPrecoTerreno.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacaoEstruturada;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author josebezerrafilho
+ */
+public class AreaPrecoTerreno {
+
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+        double metroQuadrado = sc.nextDouble();
+        
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
+
+        System.out.printf("AREA = %.2f%n", area);
+        System.out.printf("PRECO = %.2f%n", preco);
+    }
+
+}
---
+++ ProjetosNetbeans/programacaoEstruturada/ProgramacaoEstruturada/src/programacaoEstruturada/CondicaoTernaria.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacaoEstruturada;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author josebezerrafilho
+ */
+public class CondicaoTernaria {
+    public static void main(String[] args) {
+        
+        float preco = 34.5f;
+        float desconto = (preco < 20.0f) ? preco * 0.1f : preco * 0.05f;
+        
+        System.out.println(desconto);
+    }
+}
